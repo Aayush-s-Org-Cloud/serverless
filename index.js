f74aa64a7a23a42f4a121c79f2429513fa5a6718
Aayush Patel <patel.aayush2@northeastern.edu>
2024-12-02 08:25:47 -0500

    aws secrets

--- index.js
+++ index.js
@@ -3,11 +3,31 @@ const sgMail = require('@sendgrid/mail');
 // Retrieve environment variables
 const FROM_EMAIL = process.env.FROM_EMAIL;
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+const secretsManager = new AWS.SecretsManager();
+let cachedSecrets = null;
+const getEmailCredentials = async () => {
+    if (cachedSecrets) {
+        return cachedSecrets;
+    }
+    const secretName = process.env.EMAIL_CREDENTIALS_SECRET_NAME;
+    if (!secretName) {
+        throw new Error('EMAIL_CREDENTIALS_SECRET_NAME environment variable is not set.');
+    }
+    const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
+    if ('SecretString' in data) {
+        cachedSecrets = JSON.parse(data.SecretString);
+        return cachedSecrets;
+    }
+    throw new Error('SecretString not found in Secrets Manager');
+};
+
 
 exports.handler = async (event) => {
     try {
         console.log('Lambda function invoked with event:', JSON.stringify(event));
-
+        const secrets = await getEmailCredentials();
+        const FROM_EMAIL = secrets.from_email;
+        const SENDGRID_API_KEY = secrets.sendgrid_api_key;
         // 1. Parse the SNS message
         if (!event.Records || event.Records.length === 0) {
             throw new Error('No Records found in the event.');
